8829a4c65b83071e9fc1c4df8017d5fe3a2551aa
djyea <djyea@LAPTOP-KHCK215I>
2022-09-21 10:04:42 -0400

    Java basics commit 2

---
+++ Java_Basics/src/check_if_number_is_even_or_odd.java
@@ -0,0 +1,20 @@
+
+public class check_if_number_is_even_or_odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		check(2);
+		check(3);
+		
+	}
+	
+	static void check(int num) {
+		if(num % 2 == 0) {
+			System.out.println("number is even");
+		}
+		else {
+			System.out.println("number is odd");
+		}
+	}
+
+}
