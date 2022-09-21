9ec3797172faecca43830697e8148a486d810862
djyea <djyea@LAPTOP-KHCK215I>
2022-09-21 09:59:13 -0400

    java basics commit

--- Day2_app/src/com/app/Simple.java
+++ Day2_app/src/com/app/Simple.java
@@ -1,5 +1,21 @@
 package com.app;
 
+class A{
+	int x = 200;
+	static int y = 200;
+	void show() {
+		System.out.println(x + " " + y);
+		x++; y++;
+	}
+	
+}
+
 public class Simple {
-//
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		A a1 = new A(); a1.show();
+		A a2 = new A(); a2.show();
+		A a3 = new A(); a3.show();
+	}
 }
---
+++ Java_Basics/src/Print_your_name.java
@@ -0,0 +1,9 @@
+
+public class Print_your_name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Dan");
+	}
+
+}
