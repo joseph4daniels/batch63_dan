5805bd5ed982f0548151cafb668456da5096b905
djyea <djyea@LAPTOP-KHCK215I>
2022-09-21 14:29:01 -0400

    check prime

---
+++ Java_Basics/src/check_prime.java
@@ -0,0 +1,13 @@
+
+public class check_prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		if(i == 1 || i == 3 || i == 5 ||
+		   i == 7 || i == 9) {
+		   System.out.println(i + " is prime");
+		}
+	}
+
+}
