48e3db43c96947f33209f70f3f934b131950a7c7
djyea <djyea@LAPTOP-KHCK215I>
2022-09-21 11:13:13 -0400

    Java basic commit 7

--- Java_Basics/src/swap_two_numbers.java
+++ Java_Basics/src/swap_two_numbers.java
@@ -1,29 +1,29 @@
 
 class A{
-	static int a = 0;
-	static int b = 1;
 	
-	void swap() {
-		System.out.println(a + "," + b);
+	void swap(int a, int b) {
 		
 		int temp1 = a;
 		int temp2 = b;
 		b = temp1;
 		a = temp2;
 		
-		System.out.println(a + "," + b);
-
+		System.out.println("a is " + a + ", b is " + b);
 	}
 }
 
 public class swap_two_numbers {
- 	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-		A a1 = new A();
-		a1.swap();
 		
+		int a = 2;
+		int b = 3;
+		System.out.println("a is " + a + ", b is " + b);
+		
+		A a1 = new A();
+		a1.swap(a, b);
+
 	}
 	
 }
